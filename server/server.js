5c08abd8734d4f19e8210d5abbd82e6699aae7be
AKhil Sharma <akhilkatyan25b@gmail.com>
2025-02-24 19:07:21 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
-require('dotenv').config()
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+import dotenv from 'dotenv';
+dotenv.config();
 const express=require('express');
 const app=express()
 const cors=require('cors')
-const PORT=3000
+const PORT=process.env.PORT ||3000
 const authRouter=require('./routers/auth_router')
 require('./models/user_model')
 const connectDb=require("./utils/db");
@@ -10,6 +14,10 @@ const errorMiddleware = require('./middlewares/err_middleware');
 const contactRoute=require("./routers/contact_rou");
 const servicesRoute = require('./routers/service-rout');
 const adminRoute=require('./routers/admin-router')
+const path=require("path")
+
+
+const __dirname=path.resolve()
 //handle cors
 const corsOptions={
     origin:"http://localhost:5173",
@@ -27,6 +35,11 @@ app.use("/api/data",servicesRoute)
 
 //admin
 app.use("/api/admin",adminRoute)
+
+app.use(express.static(path.join(__dirname,"/Portfolio/dist")))
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"Portfolio","dist","index.html"))
+})
 
 app.use(errorMiddleware);
 connectDb().then(()=>{
